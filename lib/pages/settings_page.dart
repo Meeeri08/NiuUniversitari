ceec0b08e4700778e567acea08850e0f709d8dab
Meeeri08 <61042895+Meeeri08@users.noreply.github.com>
2023-04-25 01:59:02 +0200

    Added markers and example

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,10 +10,16 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   static const _initialCameraPosition = CameraPosition(
     target: LatLng(41.38859447609031, 2.1686747276829004),
-    zoom: 11.5,
+    zoom: 13,
   );
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   late GoogleMapController _googleMapController;
+
+  final houseTitle = "Beautiful House";
+  final housePrice = "\$1000";
+  final houseImageUrl =
+      "https://img.staticmb.com/mbcontent//images/uploads/2022/12/Most-Beautiful-House-in-the-World.jpg";
 
   @override
   void dispose() {
@@ -30,6 +37,92 @@ class _SettingsPageState extends State<SettingsPage> {
             zoomControlsEnabled: false,
             initialCameraPosition: _initialCameraPosition,
             onMapCreated: (controller) => _googleMapController = controller,
+            markers: {
+              Marker(
+                markerId: MarkerId("1"),
+                position: LatLng(41.411480550012854, 2.158319031193987),
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        height: 300,
+                        child: Column(
+                          children: [
+                            Stack(
+                              alignment: Alignment.centerLeft,
+                              children: [
+                                Image.network(
+                                  houseImageUrl,
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.arrow_back),
+                                  onPressed: () => Navigator.of(context).pop(),
+                                ),
+                              ],
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          houseTitle,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                        Text(
+                                          housePrice,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 10),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(Icons.king_bed),
+                                            SizedBox(width: 5),
+                                            Text("3 bedrooms"),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.bathtub),
+                                            SizedBox(width: 5),
+                                            Text("2 bathrooms"),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            },
           ),
           SafeArea(
             child: Padding(
