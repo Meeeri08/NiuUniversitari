0242efe7d240f2b52aa0d6db73c5b71e47be039f
Meeeri08 <61042895+Meeeri08@users.noreply.github.com>
2023-04-25 23:37:26 +0200

    mapa de la zona ara a cada objecte screen

--- lib/pages/house_detail_screen.dart
+++ lib/pages/house_detail_screen.dart
@@ -1,11 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HouseDetailScreen extends StatelessWidget {
+class HouseDetailScreen extends StatefulWidget {
   final String houseId;
 
   const HouseDetailScreen({Key? key, required this.houseId}) : super(key: key);
+
+  @override
+  _HouseDetailScreenState createState() => _HouseDetailScreenState();
+}
+
+class _HouseDetailScreenState extends State<HouseDetailScreen> {
+  late final LatLng _initialLatLng;
+  GoogleMapController? _mapController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the initial LatLng value to the location of the house
+    FirebaseFirestore.instance
+        .collection('houses')
+        .doc(widget.houseId)
+        .get()
+        .then((snapshot) {
+      final latLng = snapshot.get('latlng');
+      final lat = latLng.latitude;
+      final lng = latLng.longitude;
+      setState(() {
+        _initialLatLng = LatLng(lat, lng);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,40 +41,90 @@ class HouseDetailScreen extends StatelessWidget {
           title: Text('House Detail'),
         ),
         body: StreamBuilder<DocumentSnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('houses')
-              .doc(houseId)
-              .snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
-            final house = snapshot.data!;
-            final imageUrl = house.get('image_url');
-            final nRooms = house.get('n_rooms');
-            final nBathroom = house.get('n_bathroom');
-            final price = house.get('price');
-            final title = house.get('title');
-            //final description = house.get('description');
-            final latLng = house.get('latlng');
+            stream: FirebaseFirestore.instance
+                .collection('houses')
+                .doc(widget.houseId)
+                .snapshots(),
+            builder: (BuildContext context,
+                AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              final house = snapshot.data!;
+              final imageUrl = house.get('image_url');
+              final nRooms = house.get('n_rooms');
+              final nBathroom = house.get('n_bathroom');
+              final price = house.get('price');
+              final title = house.get('title');
+              final latLng = house.get('latlng');
 
-            // Return a widget that displays the house details
-            return Container(
-              child: Column(
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(title),
-                  // Text(description),
-                  Text('Rooms: $nRooms'),
-                  Text('Bathrooms: $nBathroom'),
-                  Text('Price: $price'),
+                  Container(
+                    height: 300,
+                    child: Stack(
+                      children: [
+                        GoogleMap(
+                          initialCameraPosition:
+                              CameraPosition(target: _initialLatLng, zoom: 15),
+                          onMapCreated: (GoogleMapController controller) {
+                            if (_mapController == null) {
+                              _mapController = controller;
+                            }
+                          },
+                          markers: {
+                            Marker(
+                              markerId: MarkerId(widget.houseId),
+                              position: _initialLatLng,
+                              infoWindow: InfoWindow(title: title),
+                            ),
+                          },
+                        ),
+                        Positioned(
+                          bottom: 16,
+                          right: 16,
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 1,
+                                  blurRadius: 5,
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  '\$$price',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(
+                                  'Per Month',
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
-              ),
-            );
-          },
-        ));
+              );
+            }));
   }
 }
